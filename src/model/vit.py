64ae206a6abf951d1c3beb21cff6dd04d6e3efdb
Alexandr Korchemnyi <akorchemnyj@gmail.com>
2022-10-19 12:48:56 +0300

    added files

---
+++ src/model/vit.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+
+class ViT(pl.LightningModule):
+    def __init__(self, ):
+        super().__init__()
+        pass
+
+    def configure_optimizers(self):
+        pass
+
+    def training_step(self, batch, batch_idx):
+        pass
+
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def test_step(self, batch, batch_idx):
+        pass
+
