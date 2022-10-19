332340f1672632408ffe7e39ecd369819998ca1b
Alexandr Korchemnyi <akorchemnyj@gmail.com>
2022-10-19 19:56:28 +0300

    added train script

--- src/train.py
+++ src/train.py
@@ -69,10 +69,12 @@ wandb_logger = WandbLogger(project=project_name, name=f'EuroSAT s{args.seed}')
 # ------------------------------------------------------------
 
 datamodule = EuroSATDataModule(args.path_to_dataset)
+datamodule.prepare_data()
+datamodule.setup()
 
 # model
 dict_args = vars(args)
-dict_args['steps_per_epoch'] = len()
+dict_args['steps_per_epoch'] = len(EuroSATDataModule.train_dataloader())
 vit = ViT(**dict_args)
 state_dict = torch.load(args.sa_state_dict)
 # ------------------------------------------------------------
