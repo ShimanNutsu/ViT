4bd9ebdf37584b21deea56b9980830b0f0022fa4
Alexandr Korchemnyi <akorchemnyj@gmail.com>
2022-10-20 10:36:41 +0300

    Fixed

--- src/modules/transformer.py
+++ src/modules/transformer.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+
 class Transformer(nn.Module):
     def __init__(self, depth, dim, num_heads=8, mlp_ratio=4, drop_rate=0.):
         super().__init__()
@@ -13,6 +14,7 @@ class Transformer(nn.Module):
             x = block(x)
         return x
 
+
 class Block(nn.Module):
     def __init__(self, dim, num_heads=8, mlp_ratio=4, drop_rate=0.):
         super().__init__()
@@ -20,7 +22,6 @@ class Block(nn.Module):
         self.attention = Attention(dim, num_heads, drop_rate, drop_rate)
         self.mlp = MLP(dim, dim * mlp_ratio, dim, drop_rate)
         self.norm_pos = nn.LayerNorm(dim)
-
 
     def forward(self, x):
         xq = self.norm_pre(x)
@@ -30,21 +31,22 @@ class Block(nn.Module):
         xn = self.mlp(xn)
         return x + xn
 
+
 class Attention(nn.Module):
-    def __init__(self, dim, num_heads=8, attn_dropout=0., proj_dropout=0.):
+    def __init__(self, input_dim, hidden_dim, num_heads=8, attn_dropout=0., proj_dropout=0.):
         super().__init__()
         self.num_heads = num_heads
-        self.scale = 1./dim**0.5
+        self.scale = 1. / hidden_dim ** 0.5
 
-        self.q = nn.ModuleList([nn.Linear(dim, dim) for _ in range(num_heads)])
-        self.k = nn.ModuleList([nn.Linear(dim, dim) for _ in range(num_heads)])
-        self.v = nn.ModuleList([nn.Linear(dim, dim) for _ in range(num_heads)])
+        self.q = nn.ModuleList([nn.Linear(input_dim, hidden_dim) for _ in range(num_heads)])
+        self.k = nn.ModuleList([nn.Linear(input_dim, hidden_dim) for _ in range(num_heads)])
+        self.v = nn.ModuleList([nn.Linear(input_dim, hidden_dim) for _ in range(num_heads)])
 
         self.attn_dropout = nn.Dropout(attn_dropout)
         self.proj_dropout = nn.Dropout(proj_dropout)
 
         self.softmax = nn.Softmax(dim=1)
-        self.out = nn.Linear(num_heads * dim, dim)
+        self.out = nn.Linear(num_heads * hidden_dim, input_dim)
 
     def forward(self, x):
         q = [self.proj_dropout(proj(x)) for proj in self.q]
@@ -52,10 +54,11 @@ class Attention(nn.Module):
         v = [self.proj_dropout(proj(x)) for proj in self.v]
         heads = [self.softmax(self.scale *
                               q[i].bmm(torch.transpose(k[i], 1, 2))).bmm(v[i])
-                              for i in range(self.num_heads)]
+                 for i in range(self.num_heads)]
         x = torch.cat(heads, dim=2)
         x = self.attn_dropout(self.out(x))
         return x
+
 
 class MLP(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None,
