e72485c1126e524c62bc6cb9f75458677bb024df
Alexandr Korchemnyi <akorchemnyj@gmail.com>
2022-10-27 11:18:21 +0300

    changed name

--- src/train.py
+++ src/train.py
@@ -75,7 +75,7 @@ datamodule.setup()
 # model
 print(args.path_to_dataset)
 dict_args = vars(args)
-dict_args['steps_per_epoch'] = len(datamodule.val_dataloader())
+dict_args['steps_per_epoch'] = len(datamodule.train_dataloader())
 vit = ViT(**dict_args)
 # ------------------------------------------------------------
 # Callbacks
